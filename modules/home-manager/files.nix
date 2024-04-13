ee9eba535a1905091c6b15c6fc1baa6b14c0b992
Babbie <11524583+Babbie@users.noreply.github.com>
2024-04-13 20:53:28 +0200

    Add kanshi

--- modules/home-manager/files.nix
+++ modules/home-manager/files.nix
@@ -19,14 +19,16 @@ with lib; {
     home.file.".scripts" = {
       enable = true;
       source = ../../scripts;
+      recursive = true;
     };
-    home.file.".scripts/wallpapers" = {
+    home.file.".scripts/wallpaper" = {
       enable = true;
       text = '' 
         #!/usr/bin/env bash
 	pkill swaybg
 	swaybg -i $(find ${config.home.homeDirectory}/.wallpapers/. -type f | shuf -n1) -m fill > /dev/null 2>&1 &
       '';
+      executable = true;
     };
   };
 }
