96ac6b7be13388548449bd6eb355518f1b290f34
Babbie <11524583+Babbie@users.noreply.github.com>
2024-04-13 20:41:08 +0200

    AUTO: Generated imports.nix

--- modules/home-manager/imports.nix
+++ modules/home-manager/imports.nix
@@ -8,8 +8,8 @@
     ./wms/dunst.nix
     ./wms/waybar.nix
     ./wms/gtk.nix
-    ./wms/kanshi.nix
     ./wms/hyprland.nix
+    ./wms/kanshi.nix
     ./defaults/tty.nix
     ./defaults/git.nix
     ./defaults/packages.nix
