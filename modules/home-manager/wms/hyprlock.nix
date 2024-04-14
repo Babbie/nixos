5192e26e5cdbec45e11febf3443bdd002772211d
Babbie <bab@bab.cloud>
2024-04-14 14:30:45 +0200

    Add hyprlock

--- modules/home-manager/wms/hyprlock.nix
+++ modules/home-manager/wms/hyprlock.nix
@@ -28,7 +28,7 @@ with lib; {
 	  position = 0, 100
 	  halign = center
 	  valign = center
-	  text = cmd[update:500] echo $ATTEMPTS[$ATTEMPTS failed attempts!]
+	  text = cmd[update:500] $ATTEMPTS[$ATTEMPTS failed attempts!]
 	  color = rgb(${config.colorScheme.palette.base08})
 	  font_size = 28
 	  font_family = Fira Sans
