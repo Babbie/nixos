22a005529c88b4a0dfd5480daa4b25e2f5f60b13
Babbie <bab@bab.cloud>
2024-04-14 16:57:36 +0200

    Update hyprlock

--- modules/home-manager/wms/hyprlock.nix
+++ modules/home-manager/wms/hyprlock.nix
@@ -33,15 +33,15 @@ with lib; {
 
 	background {
           monitor =
-	  #path = screenshot
-          color = rgba(20, 20, 20, 0.7)
+	  path = screenshot
+          #color = rgba(20, 20, 20, 0.7)
 
 	  blur_passes = 2
 	  blur_size = 3
-	  noise = 0.0117
-	  contrast = 0.8916
-	  brightness = 0.8172
-	  vibrancy = 0.1696
+	  noise = 0.2
+	  contrast = 0.9
+	  brightness = 0.8
+	  vibrancy = 0.2
 	  vibrancy_darkness = 0.0
 	}
 
