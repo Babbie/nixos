524170263cd5c55c990c256a5c166ae7dc307d99
Babbie <bab@bab.cloud>
2024-04-14 16:22:23 +0200

    Update hyprlock

--- modules/home-manager/wms/hyprlock.nix
+++ modules/home-manager/wms/hyprlock.nix
@@ -38,6 +38,11 @@ with lib; {
 
 	  blur_passes = 8
 	  blur_size = 8
+	  noise = 0.0117
+	  contrast = 0.8916
+	  brightness = 0.8172
+	  vibrancy = 0.1696
+	  vibrancy_darkness = 0.0
 	}
 
 	label {
