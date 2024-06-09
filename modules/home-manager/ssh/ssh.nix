420b9782763b7d8b5f334c28dc2cf36a15aed428
Babbie <bab@bab.cloud>
2024-06-09 19:33:31 +0200

    Fix ssh for waddle-doo

--- modules/home-manager/ssh/ssh.nix
+++ modules/home-manager/ssh/ssh.nix
@@ -9,17 +9,20 @@ with lib; {
       default = true;
       description = "Enable default ssh settings";
     };
+    userSsh.matchBlocks = mkOption {
+      default = {
+        "wilhelm" = {
+          hostname = "10.0.0.100";
+          port = 49999;
+        };
+      };
+    };
   };
 
   config = mkIf config.userSsh.enable {
     programs.ssh = {
       enable = true;
-      matchBlocks = {
-        "wilhelm" = {
-          hostname = "10.0.0.100";
-	  port = 49999;
-	};
-      };
+      matchBlocks = config.userSsh.matchBlocks;
     };
   };
 }
