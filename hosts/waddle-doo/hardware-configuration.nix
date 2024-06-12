389f4df015b9ae7cf81b05818dcd75639afb0261
Babbie <11524583+Babbie@users.noreply.github.com>
2024-06-12 20:47:11 +0200

    Add nvidia drivers to waddle-doo

--- hosts/waddle-doo/hardware-configuration.nix
+++ hosts/waddle-doo/hardware-configuration.nix
@@ -48,10 +48,14 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
+    nvidiaSettings = true;
     modesetting.enable = true;
 
     prime = {
-      sync.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
