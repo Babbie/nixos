0ae571da0b588640fcce696f471c2a6befa3d36e
Babbie <bab@bab.cloud>
2024-06-12 20:42:59 +0200

    Add nvidia drivers to waddle-doo

--- hosts/waddle-doo/hardware-configuration.nix
+++ hosts/waddle-doo/hardware-configuration.nix
@@ -51,10 +51,7 @@
     modesetting.enable = true;
 
     prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
+      sync.enable = true;
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
