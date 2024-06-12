0ab08040171ba80eaa853c35cab155be56aac7ea
Babbie <bab@bab.cloud>
2024-06-12 22:32:09 +0200

    AUTO: Generated imports.nix

--- modules/home-manager/imports.nix
+++ modules/home-manager/imports.nix
@@ -16,12 +16,12 @@
     ./wms/ags.nix
     ./wms/dunst.nix
     ./wms/gnome-keyring.nix
-    ./wms/gtk.nix
     ./wms/hypridle.nix
-    ./wms/hyprland.nix
     ./wms/hyprlock.nix
     ./wms/kanshi.nix
     ./wms/waybar.nix
     ./wms/wlsunset.nix
+    ./wms/gtk.nix
+    ./wms/hyprland.nix
   ];
 }
