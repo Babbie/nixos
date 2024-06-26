bd1fde7b08e3840628d77388766856cd2e861f6d
Babbie <11524583+Babbie@users.noreply.github.com>
2024-06-26 10:17:00 +0200

    Cursor fix

--- modules/home-manager/wms/gtk.nix
+++ modules/home-manager/wms/gtk.nix
@@ -18,27 +18,33 @@ with lib; {
         name = "Catppuccin-Macchiato-Standard-Green-Dark";
         package = pkgs.catppuccin-gtk.override {
           accents = [ "green" ];
-	  tweaks = [ "rimless" ];
-	  variant = "macchiato";
-	};
+          tweaks = [ "rimless" ];
+          variant = "macchiato";
+        };
       };
       cursorTheme = {
         name = "Catppuccin-Macchiato-Dark-Cursors";
-	package = pkgs.catppuccin-cursors.macchiatoDark;
-	size = 32;
+        package = pkgs.catppuccin-cursors.macchiatoDark;
+        size = 32;
       };
       iconTheme = {
         name = "Papirus-Dark";
         package = pkgs.catppuccin-papirus-folders.override {
-	  flavor = "macchiato";
-	  accent = "green";
-	};
+          flavor = "macchiato";
+          accent = "green";
+        };
       };
     };
     xdg.configFile = {
       "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
       "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
       "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+    };
+    home.pointerCursor = {
+      gtk.enable = true;
+      name = "Catppuccin-Macchiato-Dark-Cursors";
+      package = pkgs.catppuccin-cursors.macchiatoDark;
+      size = 32;
     };
   };
 }
