8d72359663ffaedbb8723dfacd06d3d25074a6b6
Babbie <11524583+Babbie@users.noreply.github.com>
2024-07-02 13:09:58 +0200

    AUTO: Generated imports.nix

--- modules/nixos/imports.nix
+++ modules/nixos/imports.nix
@@ -2,19 +2,19 @@
 
 {
   imports = [
+    ./defaults/audio.nix
+    ./defaults/autoUpgrade.nix
     ./defaults/babUser.nix
-    ./defaults/locale.nix
-    ./defaults/plymouth.nix
-    ./defaults/networkmanager.nix
-    ./defaults/sudo.nix
+    ./defaults/bluetooth.nix
+    ./defaults/defaultBoot.nix
+    ./defaults/gnome-keyring.nix
     ./defaults/greetd.nix
     ./defaults/homeManagerDefaults.nix
-    ./defaults/audio.nix
-    ./defaults/bluetooth.nix
-    ./defaults/misc.nix
-    ./defaults/autoUpgrade.nix
-    ./defaults/gnome-keyring.nix
+    ./defaults/locale.nix
+    ./defaults/networkmanager.nix
     ./defaults/packages.nix
-    ./defaults/defaultBoot.nix
+    ./defaults/plymouth.nix
+    ./defaults/sudo.nix
+    ./defaults/misc.nix
   ];
 }
