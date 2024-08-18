57362f8924786576dc35c748a712f49852b05e57
Babbie <bab@bab.cloud>
2024-08-18 10:38:20 +0200

    Add libgtop

--- modules/home-manager/wms/ags.nix
+++ modules/home-manager/wms/ags.nix
@@ -8,7 +8,7 @@ with lib; {
   };
 
   config = mkIf config.ags.enable {
-    home.packages = [ pkgs.bun pkgs.sassc pkgs.libgtop ];
+    home.packages = [ pkgs.bun pkgs.sassc ];
 
     programs.ags = {
       enable = true;
