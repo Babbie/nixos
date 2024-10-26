e7a6f3421a95f33c7723f4311d40823f7c136d68
Babbie <11524583+Babbie@users.noreply.github.com>
2024-10-26 15:31:57 +0200

    Add flutter dev environment

--- hosts/waddle-dee/home.nix
+++ hosts/waddle-dee/home.nix
@@ -9,6 +9,7 @@
   hyprland.enable = true;
   jsDev.enable = true;
   dotNetDev.enable = true;
+  flutterDev.enable = true;
   # azureDev.enable = true;
 
   # The home.packages option allows you to install Nix packages into your
