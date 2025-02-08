7d093aec32b198365bf74369bafa99ebfed2a29c
Babbie <bab@bab.cloud>
2025-02-08 15:21:13 +0100

    AUTO: Generated imports.nix

--- modules/home-manager/imports.nix
+++ modules/home-manager/imports.nix
@@ -2,28 +2,28 @@
 
 {
   imports = [
-    ./shells/zsh.nix
     ./browsers/firefox.nix
-    ./wms/wlsunset.nix
-    ./wms/dunst.nix
-    ./wms/gtk.nix
-    ./wms/waybar.nix
-    ./wms/hyprlock.nix
-    ./wms/hypridle.nix
-    ./wms/ags.nix
-    ./wms/gnome-keyring.nix
-    ./wms/kanshi.nix
-    ./wms/hyprland.nix
-    ./defaults/tty.nix
-    ./defaults/babUser.nix
-    ./defaults/packages.nix
-    ./defaults/git.nix
     ./colors.nix
-    ./ssh/ssh.nix
-    ./files.nix
-    ./dev/js.nix
+    ./defaults/babUser.nix
+    ./defaults/git.nix
+    ./defaults/packages.nix
+    ./defaults/tty.nix
     ./dev/dotnet.nix
     ./dev/azure.nix
     ./dev/flutter.nix
+    ./dev/js.nix
+    ./files.nix
+    ./shells/zsh.nix
+    ./ssh/ssh.nix
+    ./wms/dunst.nix
+    ./wms/hypridle.nix
+    ./wms/waybar.nix
+    ./wms/wlsunset.nix
+    ./wms/gtk.nix
+    ./wms/hyprlock.nix
+    ./wms/ags.nix
+    ./wms/gnome-keyring.nix
+    ./wms/hyprland.nix
+    ./wms/kanshi.nix
   ];
 }
