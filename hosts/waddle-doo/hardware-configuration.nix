a786bc74af237c9991a6fe2e4376e9eb94c87e2b
Babbie <bab@bab.cloud>
2025-02-08 18:48:06 +0100

    Attempt hdmi screen fix

--- hosts/waddle-doo/hardware-configuration.nix
+++ hosts/waddle-doo/hardware-configuration.nix
@@ -57,7 +57,7 @@
     open = true;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
       version = "555.58.02";
-      sha256_64bit = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
       sha256_aarch64 = "sha256-0000000000000000000000000000000000000000001=";
       openSha256 = "sha256-0000000000000000000000000000000000000000002=";
       settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
