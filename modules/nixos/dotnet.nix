cbb79f3d7891a11cfde94bca496fa68a95bd9330
Babbie <11524583+Babbie@users.noreply.github.com>
2025-02-08 15:28:30 +0100

    Update dotnet

--- modules/nixos/dotnet.nix
+++ modules/nixos/dotnet.nix
@@ -11,9 +11,8 @@ with lib; {
   config = mkIf config.dotnet.enable {
     environment = let 
       dotnet-combined = (with pkgs.dotnetCorePackages; combinePackages [
+          sdk_9_0
           sdk_8_0
-          sdk_7_0
-          sdk_6_0
       ]).overrideAttrs (finalAttrs: previousAttrs: {
         # This is needed to install workload in $HOME
         # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
