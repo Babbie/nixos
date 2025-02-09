b24f3de9a5bb229b2fdc337165ede889abd55c9b
Babbie <bab@bab.cloud>
2025-02-09 17:29:33 +0100

    Attempt hyprland lagginess fix

--- hosts/waddle-doo/hardware-configuration.nix
+++ hosts/waddle-doo/hardware-configuration.nix
@@ -55,7 +55,7 @@
     nvidiaSettings = true;
     modesetting.enable = true;
     open = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    #package = config.boot.kernelPackages.nvidiaPackages.beta;
     #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #  version = "555.58";
 
@@ -65,7 +65,23 @@
     #  settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
     #  persistencedSha256 = lib.fakeSha256;
     #};
-
+    package = config.boot.kernelPackagessssss.nvidiaPackages.mkDriver {
+      version = "565.77";
+      sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
+      sha256_aarch64 = "sha256-LSAYUnhfnK3rcuPe1dixOwAujSof19kNOfdRHE7bToE=";
+      openSha256 = "sha256-Fxo0t61KQDs71YA8u7arY+503wkAc1foaa51vi2Pl5I=";
+      settingsSha256 = "sha256-VUetj3LlOSz/LB+DDfMCN34uA4bNTTpjDrb6C6Iwukk=";
+      persistencedSha256 = "sha256-wnDjC099D8d9NJSp9D0CbsL+vfHXyJFYYgU3CwcqKww=";
+      patches = [
+        ./fix-for-linux-6.13.patch
+      ];
+      patchesOpen = [
+        ./nvidia-nv-Convert-symbol-namespace-to-string-literal.patch
+          ./crypto-Add-fix-for-6.13-Module-compilation.patch
+          ./Use-linux-aperture.c-for-removing-conflict.patch
+          ./TTM-fbdev-emulation-for-Linux-6.13.patch
+      ];
+    }
     prime = {
       sync.enable = true;
 
