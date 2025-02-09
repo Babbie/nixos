1b611ed23ad7c46a60088089d5b7c19f15feac75
Babbie <11524583+Babbie@users.noreply.github.com>
2025-02-09 17:20:56 +0100

    Attempt hyprland lagginess fix

--- hosts/waddle-doo/hardware-configuration.nix
+++ hosts/waddle-doo/hardware-configuration.nix
@@ -55,7 +55,7 @@
     nvidiaSettings = true;
     modesetting.enable = true;
     open = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
     #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #  version = "555.58";
 
@@ -65,10 +65,17 @@
     #  settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
     #  persistencedSha256 = lib.fakeSha256;
     #};
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "565.77";
+      sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
+      sha256_aarch64 = "sha256-LSAYUnhfnK3rcuPe1dixOwAujSof19kNOfdRHE7bToE=";
+      openSha256 = "sha256-Fxo0t61KQDs71YA8u7arY+503wkAc1foaa51vi2Pl5I=";
+      settingsSha256 = "sha256-VUetj3LlOSz/LB+DDfMCN34uA4bNTTpjDrb6C6Iwukk=";
+      persistencedSha256 = "sha256-wnDjC099D8d9NJSp9D0CbsL+vfHXyJFYYgU3CwcqKww=";
+    };
 
     prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
+      sync.enable = true;
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
