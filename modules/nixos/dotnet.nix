7397e2054261b0e69614dfece34fde682831a334
Babbie <bab@bab.cloud>
2025-05-10 17:21:47 +0200

    Dotnet test

--- modules/nixos/dotnet.nix
+++ modules/nixos/dotnet.nix
@@ -10,29 +10,14 @@ with lib; {
 
   config = mkIf config.dotnet.enable {
     environment = let 
-      dotnet = pkgs.dotnetCorePackages.sdk_9_0.overrideAttrs (finalAttrs: previousAttrs: {
-        # This is needed to install workload in $HOME
-        # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
-        postBuild = (previousAttrs.postBuild or '''') + ''
-          for i in $out/sdk/*
-          do
-            i=$(basename $i)
-            length=$(printf "%s" "$i" | wc -c)
-            substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
-            i="$substring""00"
-            mkdir -p $out/metadata/workloads/''${i/-*}
-            touch $out/metadata/workloads/''${i/-*}/userlocal
-          done
-        '';
-      });
+      dotnet = pkgs.dotnetCorePackages.dotnet_9.sdk;
     in
     {
+      packages = [ dotnet ];
       sessionVariables = {
-        DOTNET_ROOT = "${dotnet}";
+        DOTNET_PATH = "${dotnet}/bin/dotnet";
+        DOTNET_ROOT = "${dotnet}/share/dotnet";
       };
-      systemPackages = [
-        dotnet
-      ];
     };
   };
 }
