a5bf5692289e8d58db7f41158172b9e9154a1fa3
Babbie <bab@bab.cloud>
2025-05-10 21:47:58 +0200

    Dotnet no devshell

--- hosts/waddle-doo/configuration.nix
+++ hosts/waddle-doo/configuration.nix
@@ -13,7 +13,6 @@
     ];
 
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [ "dotnet-sdk-6.0.428" "dotnet-runtime-6.0.36" ];
 
   networking.hostName = "waddle-doo"; # Define your hostname.
 
