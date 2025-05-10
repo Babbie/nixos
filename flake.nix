fe9e2a3d0b1ab1abc786d1908d0b73553d370439
Babbie <bab@bab.cloud>
2025-05-10 20:19:43 +0200

    Dotnet devshell test

--- flake.nix
+++ flake.nix
@@ -48,7 +48,13 @@
         dotnet = dotnetCorePackages.dotnet_9.sdk;
       in
       mkShell {
-        packages = [ dotnet jetbrains.rider android-studio ];
+        packages = [ 
+          dotnet
+          jetbrains.rider.overrideAttrs (finalAttrs: previousAttrs: {
+            buildInputs = previousAttrs.buildInputs + [ dotnet ];
+          })
+          android-studio
+        ];
         DOTNET_PATH = "${dotnet}/bin/dotnet";
         DOTNET_ROOT = "${dotnet}/share/dotnet";
       };
