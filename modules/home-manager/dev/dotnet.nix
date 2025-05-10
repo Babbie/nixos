cea07c86b00acabc91bff1b4141886fc2a37700c
Babbie <bab@bab.cloud>
2025-05-10 20:56:27 +0200

    Dotnet no devshell

---
+++ modules/home-manager/dev/dotnet.nix
@@ -0,0 +1,32 @@
+{ lib, config, pkgs, ... }:
+
+with lib; {
+  imports = [
+  ];
+
+  options = {
+    dotnet.enable = mkEnableOption "Enable dotnet";
+  };
+
+  config = let
+    dotnet = dotnetCorePackages.dotnet_9.sdk;
+  in
+  mkIf config.dotnet.enable {
+    packages = [ 
+      dotnet
+      pkgs.android-studio
+      pkgs.jetbrains.rider.overrideAttrs (attrs: {
+        postInstall = ''
+          mv $out/bin/rider $out/bin/.rider-toolless
+          makeWrapper $out/bin/.rider-toolless $out/bin/rider \
+            --argv0 rider \
+            --prefix PATH : "${makeBinPath [ dotnet pkgs.dotnetPackages.Nuget pkgs.mono pkgs.msbuild ]}"
+        '' + attrs.postInstall or "";
+      })
+    ];
+    sessionVariables = {
+      DOTNET_PATH = "${dotnet}/bin/dotnet";
+      DOTNET_ROOT = "${dotnet}/share/dotnet";
+    };
+  };
+}
