8d45dc4f98a98e9c5644e67210eedf2e4a400c34
Babbie <11524583+Babbie@users.noreply.github.com>
2025-09-21 16:59:32 +0200

    Home Manager, cleanup, ly

--- flake.nix
+++ flake.nix
@@ -1,17 +1,32 @@
 {
-  description = "My NixOS Config";
+  description = "Bab's NixOS Config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs@{ self, nixpkgs, ...}: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
     nixosConfigurations.waddle-dee = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-      specialArgs = { inherit inputs; };
+      modules = [ 
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+	{
+          home-manager = {
+	    useGlobalPkgs = true;
+	    useUserPackages = true;
+	    users.bab = import ./home.nix;
+	    backupFileExtension = "backup";
+            extraSpecialArgs = { inherit inputs; };
+	  };
+	}
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,28 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+
+  home.username = "bab";
+  home.homeDirectory = "/home/bab";
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+
+  ];
+
+  programs.git.enable = true;
+  programs.zen-browser.enable = true;
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+}
