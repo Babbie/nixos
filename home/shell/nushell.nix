8516ccea9a0fbbaf2b198836ffd90e7546fb4a96
Babbie <11524583+Babbie@users.noreply.github.com>
2025-10-05 22:33:16 +0200

    tweak nushell

--- home/shell/nushell.nix
+++ home/shell/nushell.nix
@@ -5,17 +5,23 @@
       settings = {
         edit_mode = "vi";
         show_banner = false;
-        always_trash = true;
+        rm.always_trash = true;
         completions = {
           algorithm = "fuzzy";
         };
         use_kitty_protocol = true;
-        termination_signal = false;
+        display_errors.termination_signal = false;
       };
-      extraConfig = ''
+      envFile.text = ''
         $env.PROMPT_INDICATOR_VI_NORMAL = "\e[1;30mN "
         $env.PROMPT_INDICATOR_VI_INSERT = ""
       '';
+      shellAliases = {
+        ll = "ls -l";
+        np = "nix-shell --run nushell -p";
+        nix-switch = "sudo nixos-rebuild switch --flake ~/.config/nixos#";
+        nix-test = "sudo nixos-rebuild test --flake ~/.config/nixos#";
+      };
     };
 
     carapace = {
