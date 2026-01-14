4f3995fe5a0951fb1526ae09b8174bdb6a59f175
Babbie <11524583+Babbie@users.noreply.github.com>
2026-01-14 17:54:22 +0100

    tweak hypridle

--- home/wms/hypridle.nix
+++ home/wms/hypridle.nix
@@ -12,18 +12,18 @@
     settings = {
         listener = [
         {
-          timeout = 150;
+          timeout = 240;
           on-timeout = "brightnessctl --device ${config.backlightDevice} -s set 10";
           on-resume = "brightnessctl --device ${config.backlightDevice} -r";
         }
 
         {
-          timeout = 180;
+          timeout = 300;
           on-timeout = "noctalia-shell ipc call lockScreen lock";
         }
 
         {
-          timeout = 210;
+          timeout = 360;
           on-timeout = ''${lib.getExe pkgs.wlr-randr} --json | ${lib.getExe pkgs.jq} '.[].name' -r | while IFS= read -r line; do wlr-randr --output "$line" --off; done'';
           on-resume = "${pkgs.kanshi}/bin/kanshictl reload";
         }
